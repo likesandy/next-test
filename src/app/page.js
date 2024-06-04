0993ceb9d125480fd861f615899aa838b2ee7c93
tao <1445709137@qq.com>
2024-06-04 09:44:24 +0800

    feat: ✨ 静态站点生成

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,19 @@
-// getServerSideProps(page router) -> server components(app router)
-// 迁移文档：https://nextjs.org/docs/app/building-your-application/upgrading/app-router-migration#server-side-rendering-getserversideprops
 async function getTodos() {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/todos`, { cache: 'no-store' })
+  // fetch 默认开启缓存（效果等价于SSG）
+  // getstaticprops -> fetch
+  // app router SSG迁移文档：https://nextjs.org/docs/app/building-your-application/upgrading/app-router-migration#static-site-generation-getstaticprops
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
   const todos = await res.json()
 
   return todos
 }
 
-export default async function Page() {
+export default async function Index() {
   const todos = await getTodos()
 
-  return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>
-          {todo.id}:{todo.title}
-        </li>
-      ))}
-    </ul>
-  )
+  return todos.map((todo) => (
+    <div key={todo.id}>
+      {todo.id}:{todo.title}
+    </div>
+  ))
 }
