2d289a0b9ed0646ae46359aa0cf734c4d6da06c5
tao <1445709137@qq.com>
2024-06-04 23:28:46 +0800

    feat: ✨ 增量静态再生

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,27 @@
+// 先构建出生产版本，再运行生产服务
+// pnpm build
+// pnpm start
 async function getTodos() {
-  // fetch 默认开启缓存（效果等价于SSG）
-  // getstaticprops -> fetch
-  // app router SSG迁移文档：https://nextjs.org/docs/app/building-your-application/upgrading/app-router-migration#static-site-generation-getstaticprops
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-  const todos = await res.json()
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${getRandomInt(100)}`, {
+    // 缓存10s数据
+    // https://nextjs.org/docs/app/api-reference/functions/fetch#optionsnextrevalidate
+    next: { revalidate: 10 },
+  })
+  const todo = await res.json()
 
-  return todos
+  return todo
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max)
 }
 
 export default async function Index() {
-  const todos = await getTodos()
+  const todo = await getTodos()
 
-  return todos.map((todo) => (
+  return (
     <div key={todo.id}>
       {todo.id}:{todo.title}
     </div>
-  ))
+  )
 }
